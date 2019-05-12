0472cfd8f1fc403ff1e84c24ba915ae93391ecf7
Ammon Carson <pianoace1315@gmail.com>
2019-05-11 19:24:51 -0600

    added all files

--- web/index.php
+++ web/index.php
@@ -1,5 +1,55 @@
 <?php
-
-  phpinfo();
-
+session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="styles.css">
+	<title>SHOP</title>
+</head>
+<body>
+<div class="itemWrapper">
+	<div class="item">
+		<h1 class="itemName">Where The Crawdads Sing</h1>
+		<p class="itemDescription">Where the Crawdads Sing is a 2018 fiction novel by Delia Owens. It has topped the The New York Times Fiction Best Sellers of 2019 for twelve separate weeks of 2019. The novel describes the life and adventures of a young girl named Kya in the swamps of North Carolina.</p>
+		<img src="https://prodimage.images-bn.com/pimages/9780735219090_p0_v10_s550x406.jpg" alt="Product Image" class="itemImage">
+		<div class="buttonArea">
+		<a href="" class="addToCart">Add To Cart</a>
+		<a href="" class="removeFromCart">Remove From Cart</a>
+		</div>
+	</div>
+	<div class="item">
+		<h1 class="itemName">Girl, Wash Your Face</h1>
+		<p class="itemDescription">Girl, Wash Your Face: Stop Believing the Lies About Who You Are So You Can Become Who You Were Meant to Be is a best-selling 2018 self-help book by Rachel Hollis. Girl, Wash Your Face is described by The Washington Post as mixing "memoir, motivational tips, Bible quotations and common-sense girl talk." </p>
+		<img src="https://bci.kinokuniya.com/jsp/images/book-img/97814/97814002/9781400216086.JPG" alt="Product Image" class="itemImage">
+		<div class="buttonArea">
+		<a href="" class="addToCart">Add To Cart</a>
+		<a href="" class="removeFromCart">Remove From Cart</a>
+		</div>
+	</div>
+	<div class="item">
+		<h1 class="itemName">Strengths Finder 2.0</h1>
+		<p class="itemDescription">A comprehensive book on 34 strengths. Find your strength today. Gallup research finds that people who use their CliftonStrengths are more engaged and productive at work and 3x more likely than others to have an excellent quality of life. The best managers boost individual and team performance using CliftonStrengths.</p>
+		<img src="https://images.gr-assets.com/books/1440946880l/56454.jpg" alt="Product Image" class="itemImage">
+		<div class="buttonArea">
+		<a href="" class="addToCart">Add To Cart</a>
+		<a href="" class="removeFromCart">Remove From Cart</a>
+		</div>
+	</div>
+	<div class="item">
+			<h1 class="itemName">Oh, the Places You'll Go!</h1>
+			<p class="itemDescription">Oh, the Places You'll Go! is a book written and illustrated by children's author Dr. Seuss. It was first published by Random House on January 22, 1990. It was his last book to be published during his lifetime. The book concerns the journey of life and its challenges.</p>
+			<img src="https://images-na.ssl-images-amazon.com/images/I/91Fwcu%2B005L._SY550_.jpg" alt="Product Image" class="itemImage">
+			<div class="buttonArea">
+			<a href="" class="addToCart">Add To Cart</a>
+			<a href="" class="removeFromCart">Remove From Cart</a>
+			</div>
+		</div>
+</div>
+<a href="shoppingCart.html" class="goToCart">Go To Cart</a>
+</body>
+</html>
